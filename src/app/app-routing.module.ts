5724579f68a718445ccf2ceba25c1fb4cd984811
Stephen <steve.ward@gmail.com>
2017-12-12 14:47:00 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogComponent } from './log/log.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PositionsComponent } from './positions/positions.component';
+import { PositionComponent } from './position/position.component';
+import { ContentComponent } from './content/content.component';
+
+const routes: Routes = [
+  { path: 'employees',    component: EmployeesComponent},
+  { path: 'employee/:id', component: EmployeeComponent},
+  { path: 'positions',    component: PositionsComponent},
+  { path: 'position/:id', component: PositionComponent},
+  { path: '/',            component: EmployeesComponent},
+  { path: '*',            component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogComponent } from './log/log.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { PositionsComponent } from './positions/positions.component';
+import { PositionComponent } from './position/position.component';
+import { ContentComponent } from './content/content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    LogComponent,
+    PageNotFoundComponent,
+    EmployeesComponent,
+    EmployeeComponent,
+    PositionsComponent,
+    PositionComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
